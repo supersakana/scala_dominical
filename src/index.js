2c943bcec19af7114502882940416a863cbc6a66
supersakana <zacwilliamson@icloud.com>
2023-01-03 12:09:12 -0600

    scripts are now being bundled and added webpack configure file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = 'Hello Webpack';
   
     return element;
   }
   
   document.body.appendChild(component());
+  
