6e96cc5df02dbfc5da1c9bc4f5967a910f242dcc
supersakana <zacwilliamson@icloud.com>
2023-01-05 10:09:34 -0600

    home page elements are now appended through javascript

--- src/print.js
+++ src/print.js
@@ -7,7 +7,11 @@ export default function buildPage() {
 function homePage(){
   const container = document.createElement('div')
   container.classList.add('flex', 'flex-col', 'lg:flex-row', 'items-center', 'justify-center', 'h-[92vh]', 'gap-x-10', 'gap-y-6', 'mx-4')
-  container.append(homeElements())
+  const elements = homeElements()
+
+  elements.forEach((e) =>{
+    container.append(e)
+  })
 
   return container
 }
@@ -27,9 +31,13 @@ function homeElements(){
   subTitle.innerText = "Resturaunt and Bar"
   subTitle.classList.add('text-2xl', 'font-logo')
 
-  titleBlock.append(title)
-  titleBlock.append(subTitle)
+  titleBlock.append(title, subTitle)
 
+  // home image
+  const homeImg = document.createElement('img')
+  homeImg.src = "../src/img/three.jpg"
+  homeImg.alt = "view"
+  homeImg.classList.add('w-[30rem]', 'lg:w-[35rem]', 'rounded-md', 'shadow-xl')
 
-  return titleBlock
+  return [titleBlock, homeImg]
 }
