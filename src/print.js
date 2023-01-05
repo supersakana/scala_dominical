e90598145971293bd78b48146405cb4684ff8f7f
supersakana <zacwilliamson@icloud.com>
2023-01-05 09:48:42 -0600

    added subtitle element to home container

--- src/print.js
+++ src/print.js
@@ -13,13 +13,22 @@ function homePage(){
 }
 
 function homeElements(){
+  // container for header text
   const titleBlock = document.createElement('div')
   titleBlock.classList.add('text-center', 'lg:text-left')
 
+  // title
   const title = document.createElement('h1')
   title.innerText = "SCALA"
   title.classList.add('text-7xl', 'lg:text-8xl', 'font-logo', 'tracking-widest')
+
+  // sub title
+  const subTitle = document.createElement('p')
+  subTitle.innerText = "Resturaunt and Bar"
+  subTitle.classList.add('text-2xl', 'font-logo')
+
   titleBlock.append(title)
+  titleBlock.append(subTitle)
 
 
   return titleBlock
